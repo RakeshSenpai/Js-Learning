204e3bca2aff9ceecff2f6506c14a9f3ef1480e9
Rakesh07778777 <bossrakesh43@gmail.com>
2025-11-17 18:05:05 +0530

    event-listener

--- Dom/dom-1.js
+++ Dom/dom-1.js
@@ -22,12 +22,18 @@ document.body.appendChild(fragement)
 */
 
 // Event addEventListener
+//here is i select the document and aslo i give him target so it will click only div.and also i make element and append it into body , if we do that in normal way then the clicks doesnot work on append element.
 
-document.addEventListener('click', e => {
-  if ( e.target.matches('div')){
+addGlobalEventListener('click', 'div' ,  e => {
     console.log('Hii')
-   }
 })
+
+function addGlobalEventListener( a , b , cb){
+    document.addEventListener(a , e => {
+        if(e.target.matches(b)) cb(e)
+    })
+
+}
 
 const newDiv = document.createElement('div')
 const body = document.querySelector('body')
