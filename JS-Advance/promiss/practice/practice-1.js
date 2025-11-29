5950ee2e8d9cb7b0f956062dcfd2ec5a9f13d86a
Rakesh07778777 <bossrakesh43@gmail.com>
2025-11-29 20:46:38 +0530

    practice-2

--- JS-Advance/promiss/practice/practice-1.js
+++ JS-Advance/promiss/practice/practice-1.js
@@ -1,11 +1,18 @@
 const task =  new Promise((resolve , reject) => {
+    let isTask = true
     setTimeout(() => {
-        resolve()
+        if(!isTask){
+            resolve('Task Completed')
+        } else{
+            reject('Something went wrong')
+        }
     } , 2000)
 })
 
-task.then(() => {
-    console.log('Task Completed')
+task.then((message) => {
+    console.log(message)
+}).catch((error) => {
+    console.log(error)
 })
 
-console.log(task)
+// console.log(task)
