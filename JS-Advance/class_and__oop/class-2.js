6b369e2e7cdf37e83425cd5aa58bcef0d24ce416
Rakesh07778777 <bossrakesh43@gmail.com>
2025-12-04 20:02:29 +0530

    class real world

---
+++ JS-Advance/class_and__oop/class-2.js
@@ -0,0 +1,29 @@
+/*
+
+class Student {
+    constructor(name, rollNo, attendence = 0){
+        this.name = name
+        this.rollNo = rollNo
+        this.attendence = attendence
+    }
+
+    markPresent(){
+        this.attendence += 1
+        this.rollNo += 1
+    }
+
+    getDetails(){
+        return `Name: ${this.name} Roll-No: ${this.rollNo} Attendence: ${this.attendence}`
+    }
+
+}
+
+const s1 = new Student('Rakesh' , 21)
+// console.log(s1.markPresent())
+
+s1.markPresent()
+s1.markPresent()
+console.log(s1.getDetails())
+
+*/
+
