88950639dab55f420dbf0133d22b3598baff3ca8
Rakesh07778777 <bossrakesh43@gmail.com>
2025-12-04 18:53:37 +0530

    Inheritance

--- JS-Advance/class_and__oop/class.js
+++ JS-Advance/class_and__oop/class.js
@@ -26,6 +26,8 @@ console.log(anotherUser.changerPass())
 
 */
 
+/*
+
 // behind the scene
 
 function User(username, email, password){
@@ -49,3 +51,37 @@ const anotherUser = new User('Rakesh' , 'awrg@google.com' , 'abcd')
 console.log(anotherUser)
 console.log(anotherUser.encryptPass())
 console.log(anotherUser.changerPass())
+*/
+
+// Inheritence
+
+class User{
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username is ${this.username}`)
+    }
+}
+
+class Teacher extends User{
+    constructor(username , email, pass){
+        super(username) // super(), it's allow us to access to User classes username
+        this.email = email
+        this.pass = pass
+    }
+
+    addCourse(){
+        console.log(`Another username is ${this.username}`)
+    }
+}
+
+const first = new User('Rakesh')
+const second = new Teacher('Rhynamo' , 'Rhynamo@gmail.com', '123')
+
+second.logMe()
+second.addCourse()
+// first.addCourse()// this will throw error, because it doesnot Inheritence from Teacher class
+console.log(second instanceof User)
+console.log(first instanceof Teacher)
