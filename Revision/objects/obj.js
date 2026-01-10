f8ed669a7f5324a2562a1d64c9ef8ea9928f987a
Rakesh07778777 <bossrakesh43@gmail.com>
2026-01-10 19:04:49 +0530

    date methods

--- Revision/objects/obj.js
+++ Revision/objects/obj.js
@@ -1,2 +1,12 @@
 //dates and times 
 
+//some date methods
+const now = new Date()
+console.log(now.getFullYear())
+console.log(now.getMonth())
+console.log(now.getDate())
+console.log(now.getHours())
+console.log(now.getMinutes())
+console.log(now.toLocaleDateString())
+console.log(now.toDateString())
+
