828701fcea639855c42217b4c30ce79816985d05
Rakesh07778777 <bossrakesh43@gmail.com>
2026-01-18 00:00:51 +0530

    note share

--- Projects/Note-Upload/pro.js
+++ Projects/Note-Upload/pro.js
@@ -1,5 +1,6 @@
 const noteContainer = document.querySelector('.note-container')
 const createElm = document.querySelector('.btn')
+const notes = document.querySelectorAll('.input-box')
 
 createElm.addEventListener('click' , () => {
     let para = document.createElement('p')
@@ -8,4 +9,22 @@ createElm.addEventListener('click' , () => {
     para.setAttribute('contenteditable' , 'true')
     img.src = '../images/delete.png'
     noteContainer.appendChild(para).appendChild(img)
+})
+
+function saveToLocalStorage(){
+    localStorage.setItem('notes' , noteContainer.innerHTML)
+}
+
+noteContainer.addEventListener('click', (e) => {
+    if(e.target.tagName === 'IMG'){
+        e.target.parentElement.remove()
+        saveToLocalStorage()
+    }
+    else if(e.target.tagName === 'P'){
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                saveToLocalStorage()
+            }
+        })
+    }
 })
