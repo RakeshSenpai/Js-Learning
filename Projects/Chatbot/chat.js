10ae54b69a27d3053d7a65e85e6e43a124c7e778
Rakesh07778777 <bossrakesh43@gmail.com>
2026-01-24 00:04:05 +0530

    user-message

--- Projects/Chatbot/chat.js
+++ Projects/Chatbot/chat.js
@@ -1,10 +1,24 @@
 const messageInput = document.querySelector('.message-input')
+const chatBody = document.querySelector('.chat-body')
 
 // handle Enter key press for sending message
+
+const createMessageElm = (content , classes) => {
+    const div = document.createElement('div')
+    div.classList.add('message' , classes)
+   div.innerHTML = content 
+   return div
+}
+
+function handleOutGoingMessage(userMessage){
+    const messageContent = `<div class="message-text">${userMessage}</div>`
+    const outGoingMessageDiv = createMessageElm(messageContent, 'user-message')
+    chatBody.appendChild(outGoingMessageDiv)
+}
 
 messageInput.addEventListener('keydown', (e) => {
     const userMessage = e.target.value.trim()
     if(e.key === 'Enter' && userMessage){
-        console.log(userMessage)
+    handleOutGoingMessage(userMessage)
     }
 })
